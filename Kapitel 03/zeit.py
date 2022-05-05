31f2cfc5849e4c807df5ef234fbfd0261b00e8fc
Michael Weigend <20935133+mweigend@users.noreply.github.com>
2022-05-05 17:42:18 +0200

    Add files via upload

---
+++ Kapitel 03/zeit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# ------------------------------------------------------
+# Dateiname: zeit.py
+#
+# Python 3
+# Kapitel 3
+# Michael Weigend, 15.05.2022
+#-----------------------------------------------------
+import time
+print("Die aktuelle Uhrzeit:")
+print(time.asctime())
+
+input()
